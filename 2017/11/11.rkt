c155d8df979d081acb4d485aa7a23b1b7d2abc98
Ben Greenman <benjaminlgreenman@gmail.com>
2017-12-13 16:45:39 -0500

    day 11: simpler formula, I still don't really understand

--- 2017/11/11.rkt
+++ 2017/11/11.rkt
@@ -22,9 +22,8 @@
   (define N (- (hash-ref hist "n" 0) (hash-ref hist "s" 0)))
   (define NE (- (hash-ref hist "ne" 0) (hash-ref hist "sw" 0)))
   (define NW (- (hash-ref hist "nw" 0) (hash-ref hist "se" 0)))
-  (define SE (- (hash-ref hist "se" 0) (hash-ref hist "nw" 0)))
   ;; ---
-  (define x (/ (+ NE SE) 2))
+  (define x (/ (- NE NW) 2))
   (define y (+ N (/ (+ NE NW) 2)))
   (abs (+ y x)))
 
