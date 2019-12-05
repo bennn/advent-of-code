738db24c75bf42cf2a1938cad8822421002a06a1
Ben Greenman <benjaminlgreenman@gmail.com>
2019-12-05 08:21:35 -0500

    2019: day5 done

--- 2019/05/05.rkt
+++ 2019/05/05.rkt
@@ -33,6 +33,7 @@
                   (cons 2 '(0 1)))))
 
 (define air-conditioner-id 1)
+(define thermal-radiator-id 5)
 
 (define (state-ref* state pc num-args param*)
   (for/list ((i (in-range num-args))
@@ -108,13 +109,90 @@
         (raise-argument-error 'part1 "opcode" opcode)])))
 
 (define (part2 input)
-  (error 'not-implemented))
+  (define state (file->state input))
+  (define in* (list thermal-radiator-id))
+  (define out* (list))
+  (let loop ((pc 0)
+             (prev* '()))
+    (define-values [opcode param*] (parse-opcode (vector-ref state pc)))
+    (cond
+      [(= opcode 99)
+       (if (null? out*)
+         #f
+         (car out*))]
+      [(= opcode 1)
+       (define num-args 3)
+       (define instr (get-instr state pc num-args))
+       (define n* (state-ref* state (+ pc 1) (- num-args 1) param*))
+       (define loc (vector-ref% state (+ pc num-args)))
+       (define res (apply + n*))
+       (state-set! state loc res)
+       (loop (+ pc 1 num-args) (cons instr prev*))]
+      [(= opcode 2)
+       (define num-args 3)
+       (define instr (get-instr state pc num-args))
+       (define n* (state-ref* state (+ pc 1) (- num-args 1) param*))
+       (define loc (vector-ref% state (+ pc num-args)))
+       (define res (apply * n*))
+       (state-set! state loc res)
+       (loop (+ pc 1 num-args) (cons instr prev*))]
+      [(= opcode 3)
+       (define in-val (begin0 (car in*) (set! in* (cdr in*))))
+       (define num-args 1)
+       (define instr (append (get-instr state pc num-args) (list in-val)))
+       (define loc (vector-ref% state (+ pc 1)))
+       (state-set! state loc in-val)
+       (loop (+ pc 1 num-args) (cons instr prev*))]
+      [(= opcode 4)
+       (define num-args 1)
+       (define instr (get-instr state pc num-args))
+       (define loc (state-ref state (+ pc 1) (car param*)))
+       (set! out* (cons loc out*))
+       (define next-pc (+ pc 1 num-args))
+       (unless (or (zero? loc) (= 99 (let-values (((a b) (parse-opcode (vector-ref% state next-pc)))) a)))
+         (raise-user-error 'part1 "test failed at ~a~n curr instr ~a~n prev instr ~a" loc instr (reverse prev*)))
+       (loop next-pc (cons instr prev*))]
+
+      [(= opcode 5)
+       (define num-args 2)
+       (define instr (get-instr state pc num-args))
+       (define n* (state-ref* state (+ pc 1) num-args param*))
+       (if (not (zero? (car n*)))
+         (loop (cadr n*) (cons instr prev*))
+         (loop (+ pc 1 num-args) (cons instr prev*)))]
+      [(= opcode 6)
+       (define num-args 2)
+       (define instr (get-instr state pc num-args))
+       (define n* (state-ref* state (+ pc 1) num-args param*))
+       (if (zero? (car n*))
+         (loop (cadr n*) (cons instr prev*))
+         (loop (+ pc 1 num-args) (cons instr prev*)))]
+
+      [(= opcode 7)
+       (define num-args 3)
+       (define instr (get-instr state pc num-args))
+       (define n* (state-ref* state (+ pc 1) (- num-args 1) param*))
+       (define loc (vector-ref% state (+ pc num-args)))
+       (define res (if (apply < n*) 1 0))
+       (state-set! state loc res)
+       (loop (+ pc 1 num-args) (cons instr prev*))]
+      [(= opcode 8)
+       (define num-args 3)
+       (define instr (get-instr state pc num-args))
+       (define n* (state-ref* state (+ pc 1) (- num-args 1) param*))
+       (define loc (vector-ref% state (+ pc num-args)))
+       (define res (if (apply = n*) 1 0))
+       (state-set! state loc res)
+       (loop (+ pc 1 num-args) (cons instr prev*))]
+      [else
+        (raise-argument-error 'part1 "opcode" opcode)])))
+
 
 (module+ main
   (require racket/cmdline)
   (command-line
     #:program "day-05"
     #:args (INPUT)
-    (printf "part1 : ~a~n" (part1 INPUT))
-    (part2 INPUT)
+    #;(printf "part1 : ~a~n" (part1 INPUT))
+    (printf "part2 : ~a~n" (part2 INPUT))
     (void)))
