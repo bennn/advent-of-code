7026527e4fd856be908d13a935d668e83614ca63
Ben Greenman <benjaminlgreenman@gmail.com>
2019-12-05 14:10:34 -0500

    2019: 05 uncomment part1 solution

--- 2019/05/05.rkt
+++ 2019/05/05.rkt
@@ -193,6 +193,6 @@
   (command-line
     #:program "day-05"
     #:args (INPUT)
-    #;(printf "part1 : ~a~n" (part1 INPUT))
+    (printf "part1 : ~a~n" (part1 INPUT))
     (printf "part2 : ~a~n" (part2 INPUT))
     (void)))
