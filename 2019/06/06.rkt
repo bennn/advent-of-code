448e77a6b221d6f7a03837c4eaee064d897a9731
Ben Greenman <benjaminlgreenman@gmail.com>
2019-12-06 00:31:49 -0500

    2019: 06 done

--- 2019/06/06.rkt
+++ 2019/06/06.rkt
@@ -1,10 +1,38 @@
 #lang racket
 
+(require graph)
+
 (define (part1 input)
-  (error 'not-implemented))
+  (define G
+    (unweighted-graph/directed
+      (with-input-from-file
+        input
+        (lambda ()
+          (for/list ((ln (in-lines)))
+            (string-split (string-trim ln) ")"))))))
+  (define-values [dist _] (bellman-ford G "COM"))
+  (apply + (hash-values dist)))
 
 (define (part2 input)
-  (error 'not-implemented))
+  (define edge*
+      (with-input-from-file
+        input
+        (lambda ()
+          (for/list ((ln (in-lines)))
+            (string-split (string-trim ln) ")")))))
+  (define G
+    (unweighted-graph/undirected
+      edge*))
+  (define pt (fewest-vertices-path G "YOU" "SAN"))
+  (- (length pt) 3) #;
+  (let loop ((pt pt))
+    (if (or (null? pt) (null? (cdr pt)))
+      0
+      (let ((a (car pt))
+            (x (cadr pt))
+            (tl (cdr pt)))
+        (+ (if (member (list x a) edge*) 1 0)
+           (loop tl))))))
 
 (module+ main
   (require racket/cmdline)
