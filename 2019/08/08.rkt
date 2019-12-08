1848d10ac370edfb3e3081aa14a93f4540fdff87
Ben Greenman <benjaminlgreenman@gmail.com>
2019-12-08 07:44:26 -0500

    2019: 08 part1 done

--- 2019/08/08.rkt
+++ 2019/08/08.rkt
@@ -3,23 +3,38 @@
 (define w 25)
 (define h 6)
 
-(define (parse in)
-  )
+(define (file->layer* w h img)
+  (define img-bits (* w h))
+  (define str (string-trim (file->string img)))
+  (define len (string-length str))
+  (let loop ((i 0))
+    (if (<= len i)
+      '()
+      (cons (for*/vector ((row (in-range h))
+                        (col (in-range w)))
+              (define chr (string-ref str (+ i (+ col (* row w)))))
+              (string->number (string chr)))
+            (loop (+ i img-bits))))))
 
-(define (file->layer* img)
-  )
+(module+
+  test
+  (require rackunit)
+  (test-case "file->layer"
+    (check-equal? (file->layer* 3 2 "test0") '(#(1 2 3 4 5 6) #(7 8 9 0 1 2)))))
 
 (define (count-bits layer n)
-  )
+  (for/sum ((i (in-vector layer))
+            #:when (= i n))
+    1))
 
 (define (part1 input)
-  (define layer* (file->layer* input))
+  (define layer* (file->layer* w h input))
   (for/fold ((acc #f)
              (zeros #f)
              #:result (* (count-bits acc 1) (count-bits acc 2)))
             ((l (in-list layer*)))
     (define num-zeros (count-bits l 0))
-    (if (or (not zeros) (< zeros num-zeros))
+    (if (or (not zeros) (< num-zeros zeros))
       (values l num-zeros)
       (values acc zeros))))
 
