859ea1d86546d9428593d386bf33b394036f9843
Ben Greenman <benjaminlgreenman@gmail.com>
2019-12-08 08:00:33 -0500

    06: delete unused code

--- 2019/06/06.rkt
+++ 2019/06/06.rkt
@@ -2,37 +2,21 @@
 
 (require graph)
 
+(define (file->edge* input)
+  (with-input-from-file input
+    (lambda ()
+      (for/list ((ln (in-lines)))
+        (string-split (string-trim ln) ")")))))
+
 (define (part1 input)
-  (define G
-    (unweighted-graph/directed
-      (with-input-from-file
-        input
-        (lambda ()
-          (for/list ((ln (in-lines)))
-            (string-split (string-trim ln) ")"))))))
+  (define G (unweighted-graph/directed (file->edge* input)))
   (define-values [dist _] (bellman-ford G "COM"))
   (apply + (hash-values dist)))
 
 (define (part2 input)
-  (define edge*
-      (with-input-from-file
-        input
-        (lambda ()
-          (for/list ((ln (in-lines)))
-            (string-split (string-trim ln) ")")))))
-  (define G
-    (unweighted-graph/undirected
-      edge*))
+  (define G (unweighted-graph/undirected (file->edge* input)))
   (define pt (fewest-vertices-path G "YOU" "SAN"))
-  (- (length pt) 3) #;
-  (let loop ((pt pt))
-    (if (or (null? pt) (null? (cdr pt)))
-      0
-      (let ((a (car pt))
-            (x (cadr pt))
-            (tl (cdr pt)))
-        (+ (if (member (list x a) edge*) 1 0)
-           (loop tl))))))
+  (- (length pt) 3))
 
 (module+ main
   (require racket/cmdline)
